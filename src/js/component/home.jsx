5297d108d6f609df371264ec5fbac782b2783791
octavio ruben flores ramirez <ruben94fr@hotmail.com>
2024-08-13 17:31:38 +0000

    avance

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark}  from '@fortawesome/free-solid-svg-icons';
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	let [tarea,setTarea] = useState("");
+	let [listaTareas,setlistaTareas] = useState([]);
+	
+	function capturarTarea(event) {
+		setTarea(event.target.value);
+		console.log(listaTareas.concat());
+		
+
+
+	}
+
+	function insertarTarea(event) {
+		if (event.key === 'Enter') {
+			setlistaTareas(listaTareas.concat(tarea));
+			setTarea("");	
+		}
+	}
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<div className="container w-50 m-auto bg-light">
+				<div className="row">
+					<div>
+						<input type="text" name="" id="" onChange={capturarTarea} onKeyDown={insertarTarea} value={tarea} />
+						<ul>
+							{listaTareas.map((item,index)=> (<li key={index}>{item} </li>))}
+						</ul>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
